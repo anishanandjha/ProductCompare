da2f811bf6c8ea87a63f46d5722d998b62fff3cd
anishanandjha <anish.a@qapitol.com>
2020-01-11 12:44:27 +0530

    Testcase changes

--- compare/src/test/java/product/compare/ProductTest.java
+++ compare/src/test/java/product/compare/ProductTest.java
@@ -15,18 +15,37 @@ public class ProductTest extends ProductPO{
 	WebDriver driver = new ChromeDriver();
 //	@BeforeClass
 //	public void setup() {
-//		//System.setProperties();
+//		//System.setProperties(WebDriver.Chrome.driver("chromedriver"));
 //		
 //	}
 	
-	@Test
-	public void productSearch() throws InterruptedException {
+	@Test (priority=1)
+	public void productSearchAmazon() throws InterruptedException {
 		driver.get("https://www.amazon.in/");
 		Thread.sleep(5000);
 		driver.findElement(By.xpath(ProductPO.AMZ_SEARCH_INPUT)).sendKeys("iPhone XR (64GB) - Yellow");
 		Thread.sleep(3000);
 		driver.findElement(By.xpath(ProductPO.AMZ_SEARCH_BUTTON)).click();
 		Thread.sleep(5000);
+		driver.findElement(By.xpath(ProductPO.AMZ_SELECT_PRODUCT)).click();
+		Thread.sleep(5000);
+		String price1 = driver.findElement(By.xpath(ProductPO.AMZ_PRODUCT_PRICE)).getText();
+				
+		Thread.sleep(10000);
+		
+		driver.get("https://www.flipkart.com/");
+		Thread.sleep(5000);
+		driver.findElement(By.xpath(ProductPO.FLK_SEARCH_INPUT)).sendKeys("iPhone XR (64GB) - Yellow");
+		Thread.sleep(3000);
+		driver.findElement(By.xpath(ProductPO.FLK_SEARCH_BUTTON)).click();
+		Thread.sleep(5000);
+		driver.findElement(By.xpath(ProductPO.FLK_SELECT_PRODUCT)).click();
+		Thread.sleep(5000);
+		String price2 = driver.findElement(By.xpath(ProductPO.FLK_PRODUCT_PRICE)).getText();
+		
+		//if((int)price1> (int)price2) {
+			
+		//}
 				
 	}
 
